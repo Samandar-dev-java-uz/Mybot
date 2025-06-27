120861bb46a646f2986e0a31327da6c83ea447e9
Samandar <soatmurotovsamandar9@gmail.com>
2025-06-27 20:05:00 +0500

    update sturukturada yozilga varyanti

--- src/main/java/uz/pdp/model/ButtenList.java
+++ src/main/java/uz/pdp/model/ButtenList.java
@@ -1,37 +1,55 @@
 package uz.pdp.model;
 
 public interface ButtenList {
+
+    String [][] start ={
+            {"\uD83D\uDE80 start" ,"\uD83D\uDD01 back"}
+    };
     String [][] meni ={
-            {"meni","basket"},
-            {"back"}
+            {"\uD83D\uDCCB Meni","\uD83E\uDDFA Savat"},
+            {"\uD83D\uDD01 back"}
     };
     String [][] foods ={
             {"osh","shurva"},
             {"qozon kabob","manti"},
             {"somsa","shashlik"},
+            { "ichimliklar","\uD83D\uDD01 back"}
             
     };
     String [][] osh ={
-            {"choy Hona oshi"," tuy oshi"}
+            {"choy Hona oshi"," tuy oshi"},
+            {"\uD83D\uDD01 back"}
 
     };
     String [][] shurva ={
-            {"shurva","tandir shurva"}
+            {"shurva","tandir shurva"},
+            {"\uD83D\uDD01 back"}
+
     };
     String [][] qozonKabob ={
             { "qozon kabob" },
+            {"\uD83D\uDD01 back"}
 
     };
     String [][] manti ={
             {"qovoqli manti "," gushtli manti"},
-            {"xonim"}
+            {"xonim","\uD83D\uDD01 back"}
     };
     String [][] somsa ={
             {" qavoqli somsa","qoshli somsa"},
-            {" kartoshkali somsa"}
+            {" kartoshkali somsa","\uD83D\uDD01 back"}
     };
     String [][] shashlik ={
             {"qiymali shashlik "," qovuqli shashlik"},
-            {"jazli shashlik"}
+            {"jazli shashlik","\uD83D\uDD01 back"}
     };
+    String [][] ichimliklar = {
+            {"cola","fanta"},
+            {"flash","pepsi"},
+            {"gorilla","flaves"},
+            {"kok choy","qora choy"},
+            {"fuysti","denay"},
+            {"\uD83D\uDD01 back"}
+    };
+
 }
--- src/main/java/uz/pdp/model/Mybot.java
+++ src/main/java/uz/pdp/model/Mybot.java
@@ -1,57 +1,146 @@
 package uz.pdp.model;
 
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.Contact;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 
+public class Mybot extends TelegramLongPollingBot {
+    static Integer back = 1;
+    SendPhoto photo = new SendPhoto();
 
-public  class Mybot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        Message message = update.getMessage();
-        Long chatId = message.getChatId();
-        String text = message.getText();
+        if (update.getMessage() != null) {
+            if (update.getMessage().hasContact()) {
+                Contact contact = update.getMessage().getContact();
+                System.out.println(contact.getFirstName() + "\t" + contact.getLastName());
+                return;
+            }
+
+            if (update.getMessage().hasText()) {
+                Message message = update.getMessage();
+                Long chatId = message.getChatId();
+                String text = message.getText();
+                SendMessage sendMessage = getSendMessage(chatId, text);
+
+
+                try {
+                    execute(sendMessage);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private SendMessage getSendMessage(Long chatId, String text){
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(chatId);
-        sendMessage.setText(" tugmachani bosing ");
-        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-        List<KeyboardRow>rowList = new ArrayList<>();
-        KeyboardRow row1 = new KeyboardRow();
-        KeyboardRow row2 = new KeyboardRow();
-        KeyboardButton button1 = new KeyboardButton();
-        KeyboardButton button2 = new KeyboardButton();
-        KeyboardButton button3= new KeyboardButton();
-        KeyboardButton button4= new KeyboardButton();
-        KeyboardButton button5= new KeyboardButton();
-        button1.setText(" qalaysan");
-        button2.setText(" yaxshimisan");
-        button3.setText(" men yaxshi");
-        button4.setText(" yaxshi");
-        button5.setText(" good");
-        row1.add(button1);
-        row1.add(button2);
-        row1.add(button3);
-        row2.add(button4);
-        row2.add(button5);
-        rowList.add(row1);
-        rowList.add(row2);
-        replyKeyboardMarkup.setKeyboard(rowList);
-        sendMessage.setReplyMarkup(replyKeyboardMarkup);
-        try {
-            execute(sendMessage);
-        } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
+        switch (text) {
+            case "/start" -> {
+                sendMessage.setText(" hush kelibsiz Choy hona botga ");
+                back = 2;
+                addButten(sendMessage, ButtenList.meni);
+            }
+            case "\uD83D\uDCCB Meni" ->{
+                sendMessage.setText("  select food or drink");
+                back =3;
+                addButten(sendMessage,ButtenList.foods);
+            }
+            case "osh" ->{
+
+                photo.setChatId(chatId);
+                photo.setCaption("Bu bizning mazali oshimiz 🍛");
+                photo.setPhoto(new InputFile("https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Osh_plov.jpg/640px-Osh_plov.jpg"));
+                try {
+                    execute(photo);
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+                sendMessage.setText("select osh");
+                addButten(sendMessage,ButtenList.osh);
+
+
+            }
+            case "shurva" ->{
+                sendMessage.setText("  select osh");
+
+                addButten(sendMessage,ButtenList.shurva);
+            }
+            case "qozon kabob" ->{
+                sendMessage.setText("  select qozon kabob");
+
+                addButten(sendMessage,ButtenList.qozonKabob);
+            }
+            case "manti" ->{
+                sendMessage.setText("  select manti");
+                addButten(sendMessage,ButtenList.manti);
+            }
+            case "somsa" ->{
+                sendMessage.setText("  select  somsa ");
+
+                addButten(sendMessage,ButtenList.somsa);
+            }
+            case "shashlik" ->{
+                sendMessage.setText("  select shashlik");
+
+                addButten(sendMessage,ButtenList.shashlik);
+            }
+            case "ichimliklar" ->{
+                sendMessage.setText("  select  drink");
+
+                addButten(sendMessage,ButtenList.ichimliklar);
+            }
+            case "\uD83D\uDD01 back" ->{
+                if (back == 3) {
+                    back = 2;
+                    sendMessage.setText("Orqaga: menyuga qaytdingiz");
+                    addButten(sendMessage, ButtenList.foods);
+                } else if (back == 2) {
+                    back = 1;
+                    sendMessage.setText("Orqaga: asosiy menyuga qaytdingiz");
+                    addButten(sendMessage, ButtenList.meni);
+                } else {
+                    sendMessage.setText("Siz bosh menyudasiz");
+                    addButten(sendMessage, ButtenList.meni);
+                }
+            }
+
         }
+        return sendMessage;
+    }
+
+    private void addButten(SendMessage sendMessage, String[][] meni) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        List<KeyboardRow> keyboardRowList = new ArrayList<>();
+
+        for (String[] strings : meni) {
+            KeyboardRow keyboardRow = new KeyboardRow();
+            for (String s : strings) {
+                keyboardRow.add(s);
+            }
+            keyboardRowList.add(keyboardRow);
+        }
+
+        replyKeyboardMarkup.setKeyboard(keyboardRowList);
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
 
     }
 
@@ -60,6 +149,7 @@ public  class Mybot extends TelegramLongPollingBot {
     public String getBotUsername() {
         return "mychoyhonabot.";
     }
+
     @Deprecated
     public String getBotToken() {
         return "8042024410:AAHphWiR33H-Q3o1cWEybfYFoWLmIzIGgus";
